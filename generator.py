0d9fbbb5c1a9fa26c28a47161f91f00c2f057022
Maiiialen <44734356+Maiiialen@users.noreply.github.com>
2022-05-07 22:05:09 +0200

    Update generating

--- generator.py
+++ generator.py
@@ -3,37 +3,41 @@ import math
 
 from numpy import correlate, greater
 from Point import Point
+from Grid import Grid
+from Robot import Robot
 
 
 def generator(file):
     lines = []
     robotPoints = []
-    
 
     # grid
-    grid_x_size = random.randint(10, 100)
-    grid_y_size = random.randint(10, 100)
+    grid_x_size = random.randint(20, 100)
+    grid_y_size = random.randint(20, 100)
     cell_size = 2000
     line = "g " + str(grid_x_size) + " " + \
         str(grid_y_size) + " " + str(cell_size)
     lines.append(line)
 
+    grid = Grid(grid_x_size, grid_y_size, cell_size, 1)
+
     # robots
-    robot_size = math.floor((random.randint(5, 9)/10) * cell_size)
-    robot_velocity = math.floor((random.randint(10, 50)))
     robots_number = random.randint(10, 100)
     for _ in range(0, robots_number):
-        notCorrectPlace = True
-        while(notCorrectPlace):
+        robot_size = math.floor((random.randint(5, 9)/10) * cell_size)
+        robot_velocity = math.floor((random.randint(100, 500)))
+        CorrectPlace = False
+        while(not CorrectPlace):
             pos_x = random.randint(robot_size//2, grid_x_size*cell_size - robot_size//2)
             pos_y = random.randint(robot_size//2, grid_y_size*cell_size - robot_size//2)
-            notCorrectPlace = False
-            for point in robotPoints:
-                distance = int(math.hypot((point.x - pos_x),(point.y - pos_y)))
-                if (distance) <= robot_size:
-                    notCorrectPlace = True
-                    print(str(distance) + ", " + str(robot_size))
-                    break
+            robot = Robot(1, 1, pos_x, pos_y, [])
+            CorrectPlace = grid.addRobotPosition(robot, Point(pos_x, pos_y))
+            # notCorrectPlace = False
+            # for point in robotPoints:
+            #     distance = int(math.hypot((point.x - pos_x),(point.y - pos_y)))
+            #     if (distance) <= robot_size:
+            #         notCorrectPlace = True
+            #         break
         robotPoints.append(Point(pos_x, pos_y))
         line = "r " + str(robot_size) + " "+ str(robot_velocity) + " " + str(pos_x) + " " + str(pos_y)
         lines.append(line)
@@ -58,18 +62,18 @@ def generatorCells(file):
     robotPoints = []
 
     # grid
-    grid_x_size = random.randint(10, 100)
-    grid_y_size = random.randint(10, 100)
+    grid_x_size = random.randint(20, 100)
+    grid_y_size = random.randint(20, 100)
     cell_size = 2000
     line = "g " + str(grid_x_size) + " " + \
         str(grid_y_size) + " " + str(cell_size)
     lines.append(line)
 
     # robots
-    robot_size = math.floor((random.randint(5, 9)/10) * cell_size)
-    robot_velocity = math.floor((random.randint(10, 50)))
     robots_number = random.randint(10, 100)
     for _ in range(0, robots_number):
+        robot_size = math.floor((random.randint(5, 9)/10) * cell_size)
+        robot_velocity = math.floor((random.randint(100, 500)))
         notCorrectPlace = True
         while(notCorrectPlace):
             pos_x = random.randrange(cell_size//2, grid_x_size*cell_size - cell_size//2, cell_size)
@@ -95,5 +99,8 @@ def generatorCells(file):
             f.write(line)
             f.write("\n")
 
-generator("grid5.txt")
-# generatorCells("grid6.txt")
+for i in range(0,1):
+    generator("generated/generated_"+str(i)+".txt")
+
+# for i in range(0,1000):
+#     generatorCells("generatedCells/generatedCells_"+str(i)+".txt")
