83b398823f2d4e5cf6a138f4d221b4f861b3d165
Maiiialen <44734356+Maiiialen@users.noreply.github.com>
2022-05-09 19:28:31 +0200

    Fix going back when blocked

--- Robot.py
+++ Robot.py
@@ -8,8 +8,10 @@ class Robot:
     position_y: int
     path: list
     blocked = 0
-    goesBack = 0
-    goesLeft = 0
+    isGoingBack = 0
+    isGoingLeft = 0
+    pathSizeAfterPop:int
+    sameSizeNumber = 0
 
     def __init__(self, size, velocity, position_x, position_y, path):
         self.size = size
@@ -19,14 +21,21 @@ class Robot:
         self.path = []
         for point in path:
             self.path.append(point)
+        self.pathSizeAfterPop = len(self.path)
 
     def calculateMove(self, method):
         if method == 0:
             if len(self.path) > 0:
                 distance_to_end_point = int(math.hypot((self.position_x - self.path[0].x), (self.position_y - self.path[0].y)))
                 if distance_to_end_point == 0:
-                    self.goesBack = 0
                     self.path.pop(0)
+                    self.isGoingBack = 0
+                    if self.pathSizeAfterPop == len(self.path):
+                        self.sameSizeNumber += 1
+                    else:
+                        self.sameSizeNumber = 0
+                        self.pathSizeAfterPop = len(self.path)
+                        # print(self.sameSizeNumber)
             if len(self.path) > 0:
                 distance_to_end_point = int(math.hypot((self.position_x - self.path[0].x), (self.position_y - self.path[0].y)))
                 if distance_to_end_point < self.velocity:
@@ -59,7 +68,12 @@ class Robot:
             if len(self.path) > 0:
                 if self.position_x - self.path[0].x == 0 and self.position_y - self.path[0].y == 0:
                     self.path.pop(0)
-                    self.goesBack = 0
+                    self.isGoingBack = 0
+                    if self.pathSizeAfterPop == len(self.path):
+                        self.sameSizeNumber += 1
+                    else:
+                        self.sameSizeNumber = 0
+                        self.pathSizeAfterPop = len(self.path)
             if len(self.path) > 0:
                 x_diff = abs(self.position_x - self.path[0].x)
                 y_diff = abs(self.position_y - self.path[0].y)
@@ -93,51 +107,50 @@ class Robot:
             print("\t", end="")
             point.printp()
 
-    def goBack(self, method):
-        if self.goesBack == 0:
-            self.goesBack = 1
+    def goBack(self, method, cellSize):
+        if self.isGoingBack == 0:
+            self.isGoingBack = 1
             if method == 0:
                 x_diff = self.position_x - self.path[0].x
                 if x_diff != 0:
                     alpha = math.atan(abs((self.position_y - self.path[0].y)/(x_diff)))
                     if self.path[0].y - self.position_y < 0 and self.path[0].x - self.position_x < 0:
-                        new_x = int(self.position_x + math.cos(alpha)*self.size)
-                        new_y = int(self.position_y + math.sin(alpha)*self.size)
+                        new_x = int(self.position_x + math.cos(alpha)*cellSize)
+                        new_y = int(self.position_y + math.sin(alpha)*cellSize)
                     elif self.path[0].y - self.position_y < 0:
-                        new_x = int(self.position_x - math.cos(alpha)*self.size)
-                        new_y = int(self.position_y + math.sin(alpha)*self.size)
+                        new_x = int(self.position_x - math.cos(alpha)*cellSize)
+                        new_y = int(self.position_y + math.sin(alpha)*cellSize)
                     elif self.path[0].x - self.position_x < 0:
-                        new_x = int(self.position_x + math.cos(alpha)*self.size)
-                        new_y = int(self.position_y - math.sin(alpha)*self.size)
+                        new_x = int(self.position_x + math.cos(alpha)*cellSize)
+                        new_y = int(self.position_y - math.sin(alpha)*cellSize)
                     else:
-                        new_x = int(self.position_x - math.cos(alpha)*self.size)
-                        new_y = int(self.position_y - math.sin(alpha)*self.size)
+                        new_x = int(self.position_x - math.cos(alpha)*cellSize)
+                        new_y = int(self.position_y - math.sin(alpha)*cellSize)
                     new_pos = Point(new_x, new_y)
                 else:
                     if self.path[0].y - self.position_y < 0:
-                        new_pos = Point(int(self.position_x), int(self.position_y + self.size))
+                        new_pos = Point(int(self.position_x), int(self.position_y + cellSize))
                     else:
-                        new_pos = Point(int(self.position_x), int(self.position_y - self.size))
+                        new_pos = Point(int(self.position_x), int(self.position_y - cellSize))
                 self.path.insert(0, new_pos)
                 return True
             else:
                 if abs(self.position_x - self.path[0].x) != 0:
                     if self.path[0].x - self.position_x < 0:
-                        new_pos = Point(int(self.position_x + self.size), int(self.position_y))
+                        new_pos = Point(int(self.position_x + cellSize), int(self.position_y))
                     else:
-                        new_pos = Point(int(self.position_x - self.size), int(self.position_y))
+                        new_pos = Point(int(self.position_x - cellSize), int(self.position_y))
                 else:
                     if self.path[0].y - self.position_y < 0:
-                        new_pos = Point(int(self.position_x), int(self.position_y + self.size))
+                        new_pos = Point(int(self.position_x), int(self.position_y + cellSize))
                     else:
-                        new_pos = Point(int(self.position_x), int(self.position_y - self.size))
+                        new_pos = Point(int(self.position_x), int(self.position_y - cellSize))
                 self.path.insert(0, new_pos)
                 return True
         return False
 
     def goLeft(self, method, cellSize):
-        self.goesBack = 1
-        self.goesLeft = 1
+        self.isGoingLeft = 1
         if method == 0:
             x_diff = self.position_x - self.path[0].x
             if x_diff != 0:
@@ -177,7 +190,7 @@ class Robot:
             return True
 
     def goRight(self, method, cellSize):
-        self.goesLeft = 0
+        self.isGoingLeft = 0
         if method == 0:
             x_diff = self.position_x - self.path[0].x
             if x_diff != 0:
@@ -217,17 +230,12 @@ class Robot:
             return True
     
     def changeSide(self, method, cellSize):
-        if self.goesBack == 1:
-            # print("path_1")
-            # print(self.path[0].printp())
-            # print(str(len(self.path)))
-            self.path.pop(0)
-            # print("path_2")
-            # print(self.path[0].printp())
-            if self.goesLeft == 0:
-                self.goLeft(method, cellSize)
-            elif self.goesLeft == 1:
-                self.goRight(method, cellSize)
+        if self.isGoingBack == 1:
+            self.removePoint()
+        if self.isGoingLeft == 0:
+            self.goLeft(method, cellSize)
+        else:
+            self.goRight(method, cellSize)
 
     def removePoint(self):
         self.path.pop(0)
