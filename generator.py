cda639cb61d0afc6bdecb0955bdca8c68abb8d1a
Maiiialen <44734356+Maiiialen@users.noreply.github.com>
2022-05-09 19:29:57 +0200

    Adapt the generator to the project requirements

--- generator.py
+++ generator.py
@@ -6,46 +6,50 @@ from Point import Point
 from Grid import Grid
 from Robot import Robot
 
+cell_size = 2000
+velocity = []
+size = []
 
-def generator(file):
+for _ in range(0, 2000):
+    velocity.append(random.randint(100, 500))
+    size.append(math.floor((random.randint(5, 8)/10) * cell_size))
+
+
+def generator(file, gridSize_x, gridSize_y, robotsNumber_percent, discs):
     lines = []
-    robotPoints = []
 
     # grid
-    grid_x_size = random.randint(20, 100)
-    grid_y_size = random.randint(20, 100)
-    cell_size = 2000
-    line = "g " + str(grid_x_size) + " " + \
-        str(grid_y_size) + " " + str(cell_size)
+    line = "g " + str(gridSize_x) + " " + \
+        str(gridSize_y) + " " + str(cell_size)
     lines.append(line)
 
-    grid = Grid(grid_x_size, grid_y_size, cell_size, 1)
+    grid = Grid(gridSize_x, gridSize_y, cell_size, 1)
 
     # robots
-    robots_number = random.randint(10, 100)
-    for _ in range(0, robots_number):
-        robot_size = math.floor((random.randint(5, 8)/10) * cell_size)
-        robot_velocity = math.floor((random.randint(100, 500)))
+    robots_number = robotsNumber_percent*gridSize_x*gridSize_y//100
+    for i in range(0, robots_number):
+        # print(robots_number)
+        robot_size = size[i]
+        robot_velocity = velocity[i]
         CorrectPlace = False
         while(not CorrectPlace):
-            pos_x = random.randint(robot_size//2, grid_x_size*cell_size - robot_size//2)
-            pos_y = random.randint(robot_size//2, grid_y_size*cell_size - robot_size//2)
-            robot = Robot(1, 1, pos_x, pos_y, [])
+            pos_x = random.randint(
+                robot_size//2, gridSize_x*cell_size - robot_size//2)
+            pos_y = random.randint(
+                robot_size//2, gridSize_y*cell_size - robot_size//2)
+            robot = Robot(robot_size, 1, pos_x, pos_y, [])
             CorrectPlace = grid.addRobotPosition(robot, Point(pos_x, pos_y))
-            # notCorrectPlace = False
-            # for point in robotPoints:
-            #     distance = int(math.hypot((point.x - pos_x),(point.y - pos_y)))
-            #     if (distance) <= robot_size:
-            #         notCorrectPlace = True
-            #         break
-        robotPoints.append(Point(pos_x, pos_y))
-        line = "r " + str(robot_size) + " "+ str(robot_velocity) + " " + str(pos_x) + " " + str(pos_y)
+        line = "r " + str(robot_size) + " " + str(robot_velocity) + \
+            " " + str(pos_x) + " " + str(pos_y)
         lines.append(line)
 
-        disc_number = random.randint(3, 10)
+        disc_number = discs
+        # disc_number = random.randint(3, 10)
         for _ in range(0, disc_number):
-            disc_x = random.randint(robot_size//2, grid_x_size*cell_size - robot_size//2)
-            disc_y = random.randint(robot_size//2, grid_y_size*cell_size - robot_size//2)
+            disc_x = random.randint(
+                robot_size//2, gridSize_x*cell_size - robot_size//2)
+            disc_y = random.randint(
+                robot_size//2, gridSize_y*cell_size - robot_size//2)
             line = "d " + str(disc_x) + " " + str(disc_y)
             lines.append(line)
 
@@ -57,38 +61,40 @@ def generator(file):
             f.write("\n")
 
 
-def generatorCells(file):
+def generatorCells(file, gridSize_x, gridSize_y, robotsNumber_percent, discs):
     lines = []
     robotPoints = []
 
     # grid
-    grid_x_size = random.randint(20, 100)
-    grid_y_size = random.randint(20, 100)
-    cell_size = 2000
-    line = "g " + str(grid_x_size) + " " + \
-        str(grid_y_size) + " " + str(cell_size)
+    line = "g " + str(gridSize_x) + " " + \
+        str(gridSize_y) + " " + str(cell_size)
     lines.append(line)
 
     # robots
-    robots_number = random.randint(10, 100)
-    for _ in range(0, robots_number):
-        robot_size = math.floor((random.randint(5, 8)/10) * cell_size)
-        robot_velocity = math.floor((random.randint(100, 500)))
+    robots_number = robotsNumber_percent*gridSize_x*gridSize_y//100
+    for i in range(0, robots_number):
+        robot_size = size[i]
+        robot_velocity = velocity[i]
         notCorrectPlace = True
         while(notCorrectPlace):
-            pos_x = random.randrange(cell_size//2, grid_x_size*cell_size - cell_size//2, cell_size)
-            pos_y = random.randrange(cell_size//2, grid_y_size*cell_size - cell_size//2, cell_size)
+            pos_x = random.randrange(
+                cell_size//2, gridSize_x*cell_size - cell_size//2, cell_size)
+            pos_y = random.randrange(
+                cell_size//2, gridSize_y*cell_size - cell_size//2, cell_size)
             notCorrectPlace = False
             for point in robotPoints:
                 if pos_x != point.x and pos_y != point.y:
                     notCorrectPlace = False
-        line = "r " + str(robot_size) + " "+ str(robot_velocity) + " " + str(pos_x) + " " + str(pos_y)
+        line = "r " + str(robot_size) + " " + str(robot_velocity) + \
+            " " + str(pos_x) + " " + str(pos_y)
         lines.append(line)
 
-        disc_number = random.randint(3, 10)
+        disc_number = discs
         for _ in range(0, disc_number):
-            disc_x = random.randrange(cell_size//2, grid_x_size*cell_size - cell_size//2, cell_size)
-            disc_y = random.randrange(cell_size//2, grid_y_size*cell_size - cell_size//2, cell_size)
+            disc_x = random.randrange(
+                cell_size//2, gridSize_x*cell_size - cell_size//2, cell_size)
+            disc_y = random.randrange(
+                cell_size//2, gridSize_y*cell_size - cell_size//2, cell_size)
             line = "d " + str(disc_x) + " " + str(disc_y)
             lines.append(line)
 
@@ -99,8 +105,18 @@ def generatorCells(file):
             f.write(line)
             f.write("\n")
 
-for i in range(0,1):
-    generator("generated/generated_"+str(i)+".txt")
 
-# for i in range(0,1000):
-#     generatorCells("generatedCells/generatedCells_"+str(i)+".txt")
+gridSize_x = [10, 50, 100, 20, 100, 40, 80]
+gridSize_y = [10, 50, 100, 100, 20, 80, 40]
+robotsNumber_percent = [5, 10, 15, 20]
+discs = [3, 10]
+
+for i in range(0, len(gridSize_x)):
+    for j in range(0, len(robotsNumber_percent)):
+        for k in range(0, len(discs)):
+            generator("results/free_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+".txt", gridSize_x[i], gridSize_y[i], robotsNumber_percent[j], discs[k])
+
+# for i in range(0, len(gridSize_x)):
+#     for j in range(0, len(robotsNumber_percent)):
+#         for k in range(0, len(discs)):
+#             generatorCells("results/grid_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+".txt"", gridSize_x[i], gridSize_y[i], robotsNumber_percent[j], discs[k])
