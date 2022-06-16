d702db1e75de1a5aada3cf0cb01d7bf41d3f055e
Maiiialen <44734356+Maiiialen@users.noreply.github.com>
2022-06-16 19:26:56 +0200

    Add new results

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from Manager_goSide import Manager_goSide
 from Manager_goSide_Grid import Manager_goSide_Grid
 from Manager_goBack import Manager_goBack
           
-gridSize_x = [70]
-gridSize_y = [70]
+gridSize_x = [20]
+gridSize_y = [20]
 # gridSize_x = [10, 20, 30, 40, 50, 60, 70]
 # gridSize_y = [10, 20, 30, 40, 50, 60, 70]
 # gridSize_x = [10, 50, 100, 20, 100, 40, 80]
 # gridSize_y = [10, 50, 100, 100, 20, 80, 40]
 #robotsNumber_percent = [10]
-robotsNumber_percent = [7]
+robotsNumber_percent = [5]
 discs = [3]
 
 
@@ -29,25 +29,25 @@ def runTest(cell_capacity, method, resource_management):
     #     f.write("\n")
     # print("Done 1")
 
-    manager = Manager_goSide("configurations/free_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+".txt", cell_capacity, method, resource_management, 0)
-    a = time.time()
-    results = manager.manage()
-    b = time.time()
-    c = b - a
-    with open("results/results_goSide_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+"_" + str(cell_capacity) + str(method) + str(resource_management) +".txt", 'a') as f:
-        f.write(results + " " + str(c))
-        f.write("\n")
-    print("Done 2")
-
-    # manager = Manager_goBack("configurations/free_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+".txt", cell_capacity, method, resource_management, 0)
+    # manager = Manager_goSide("configurations/free_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+".txt", cell_capacity, method, resource_management, 0)
     # a = time.time()
     # results = manager.manage()
     # b = time.time()
     # c = b - a
-    # with open("results/results_goBack_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+"_" + str(cell_capacity) + str(method) + str(resource_management) +".txt", 'a') as f:
+    # with open("results/results_goSide_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+"_" + str(cell_capacity) + str(method) + str(resource_management) +".txt", 'a') as f:
     #     f.write(results + " " + str(c))
     #     f.write("\n")
-    # print("Done 3")
+    # print("Done 2")
+
+    manager = Manager_goBack("configurations/free_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+".txt", cell_capacity, method, resource_management, 0)
+    a = time.time()
+    results = manager.manage()
+    b = time.time()
+    c = b - a
+    with open("results/results_goBack_"+str(gridSize_x[i])+"_"+str(gridSize_y[i])+"_"+str(robotsNumber_percent[j])+"_"+str(discs[k])+"_" + str(cell_capacity) + str(method) + str(resource_management) +".txt", 'a') as f:
+        f.write(results + " " + str(c))
+        f.write("\n")
+    print("Done 3")
 
 
 def runTest_grid(cell_capacity, method, resource_management):
@@ -86,18 +86,18 @@ if __name__ == '__main__':
     for i in range(0, len(gridSize_x)):
         for j in range(0, len(robotsNumber_percent)):
             for k in range(0, len(discs)):
-                for _ in range(0, 21):
+                for _ in range(0, 20):
                     print("_ Start " + str(number) + str(" [") + str(gridSize_x[i]) + "x" + str(gridSize_y[i]) + "_" + str(robotsNumber_percent[j]) + "_" + str(discs[k]) + str("]") + " ___")
                     print("")
 
-                    # runTest(1, 0, 0)
-                    # runTest(1, 0, 1)
-                    # runTest(2, 0, 1)
+                    runTest(1, 0, 0)
+                    runTest(1, 0, 1)
+                    runTest(2, 0, 1)
                     # runTest(1, 1, 0)
                     # runTest(1, 1, 1)
                     # runTest(2, 1, 1)
-                    runTest_grid(1, 1, 0)
-                    runTest_grid(1, 1, 1)
-                    runTest_grid(2, 1, 1)
+                    # runTest_grid(1, 1, 0)
+                    # runTest_grid(1, 1, 1)
+                    # runTest_grid(2, 1, 1)
 
                     number += 1
